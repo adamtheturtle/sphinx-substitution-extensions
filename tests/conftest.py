004bf3d64a30dea59b7dd93c9c49b90abf698360
Adam Dangoor <adamdangoor@gmail.com>
2024-08-29 12:33:36 +0100

    Switch from typeguard to beartype

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,16 @@
 """Configuration for pytest."""
 
+import pytest
+from beartype import beartype
+
 pytest_plugins = "sphinx.testing.fixtures"  # pylint: disable=invalid-name
+
+
+def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
+    """
+    Apply the beartype decorator to all collected test functions.
+    """
+    for item in items:
+        # All our tests are functions, for now
+        assert isinstance(item, pytest.Function)
+        item.obj = beartype(obj=item.obj)
