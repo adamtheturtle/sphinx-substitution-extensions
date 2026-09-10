29007fee8bd66f16411513204c641ac3f10266dc
Adam Dangoor <adamdangoor@gmail.com>
2026-09-10 14:20:31 +0100

    Type MyST parsing contexts explicitly

--- src/sphinx_substitution_extensions/__init__.py
+++ src/sphinx_substitution_extensions/__init__.py
@@ -20,7 +20,7 @@ from docutils.parsers.rst import directives
 from docutils.parsers.rst.directives.images import Image
 from docutils.parsers.rst.directives.misc import Include as DocutilsInclude
 from docutils.parsers.rst.roles import code_role
-from docutils.parsers.rst.states import Inliner
+from docutils.parsers.rst.states import Inliner, RSTState
 from docutils.statemachine import StringList
 from myst_parser.config.main import MdParserConfig
 from myst_parser.mdit_to_docutils.base import DocutilsRenderer
@@ -71,7 +71,9 @@ def _is_object_pair(
 
 
 @beartype
-def _get_myst_config(*, context: object) -> MdParserConfig | None:
+def _get_myst_config(
+    *, context: Inliner | MockInliner | MockState | RSTState
+) -> MdParserConfig | None:
     """Get the effective MyST configuration from a parsing context."""
     if isinstance(context, (MockInliner, MockState)):
         # MyST merges front matter into a document-local configuration before
